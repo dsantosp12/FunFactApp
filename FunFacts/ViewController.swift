6cda0841a7dc65a80907d6411b2707e97ac8d191
Daniel Santos <dsantosp12@gmail.com>
2017-02-18 23:47:43 -0500

    init the app

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlet means interface builder outler
+    @IBOutlet weak var funFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.funFactLabel.text = "An interesting fact!"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFact() {
+        self.funFactLabel.text = "You pressed me!"
+    }
 
 }
 
